90c9e9faebe7b614792caa6c869b6f2f57a94c9e
Alex <weiru@hotmail.ca>
2025-05-08 01:20:37 -0400

    Temporary page for vocab profile route

---
+++ client/app/(withNav)/profile/vocabulary/page.tsx
@@ -0,0 +1,16 @@
+import ProfileContainer from "../components/profileContainer";
+
+const ProfileVocabularyPage = () => {
+  return (
+    <div>
+      <ProfileContainer
+        title="My Vocabulary"
+        description="List of all saved vocabulary"
+      >
+        <div>hi</div>
+      </ProfileContainer>
+    </div>
+  );
+};
+
+export default ProfileVocabularyPage;
