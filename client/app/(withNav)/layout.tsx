d049721c2946421096e09bef0cf1c2c28f8be0c5
Alex <weiru@hotmail.ca>
2025-05-08 01:19:01 -0400

    Add container and footer

--- client/app/(withNav)/layout.tsx
+++ client/app/(withNav)/layout.tsx
@@ -1,7 +1,9 @@
+import PageContainer from "@/components/ui/page-container";
 import "../globals.css";
 import Navbar from "@/components/ui/navbar/navbar";
+import Footer from "@/components/ui/footer";
 
-export default function RootLayout({
+export default function WithNavLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -9,7 +11,12 @@ export default function RootLayout({
   return (
     <>
       <Navbar />
-      {children}
+      <main className="min-h-screen flex flex-col pb-16 md:pb-0">
+        <div className="flex-1">
+          <PageContainer>{children}</PageContainer>
+        </div>
+        <Footer />
+      </main>
     </>
   );
 }
