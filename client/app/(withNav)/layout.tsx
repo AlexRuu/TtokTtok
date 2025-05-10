3a6d6c98f1d847436e8b21c4f01cc1c50f838bed
Alex <weiru@hotmail.ca>
2025-05-09 21:37:08 -0400

    Update layout styling to ensure footer stays at bottom, even in mobile

--- client/app/(withNav)/layout.tsx
+++ client/app/(withNav)/layout.tsx
@@ -9,14 +9,12 @@ export default function WithNavLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="min-h-screen flex flex-col pb-16 md:pb-0">
-        <div className="flex-1">
-          <PageContainer>{children}</PageContainer>
-        </div>
-        <Footer />
+      <main className="flex-1">
+        <PageContainer>{children}</PageContainer>
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
