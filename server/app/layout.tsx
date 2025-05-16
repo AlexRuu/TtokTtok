9064a565a58735e18e2c60799fd2fe9b5f4f7054
Alex <weiru@hotmail.ca>
2025-05-16 18:56:35 -0400

    Begin working on admin pages

--- server/app/layout.tsx
+++ server/app/layout.tsx
@@ -1,30 +1,42 @@
-import { Geist } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+import Navbar from "@/components/navbar/navbar";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
-
-const geistSans = Geist({
+export const pretendard = localFont({
+  src: [
+    {
+      path: "./fonts/PretendardVariable.woff2",
+      weight: "100 900",
+      style: "normal",
+    },
+  ],
   display: "swap",
-  subsets: ["latin"],
+  variable: "--font-pretendard",
 });
 
-export default function RootLayout({
+export const metadata: Metadata = {
+  title: "Ttok Ttok Admin",
+  description: "Ttok Ttok App Admin",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        {children}
+    <html lang="en" className={pretendard.variable}>
+      <body>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   );
