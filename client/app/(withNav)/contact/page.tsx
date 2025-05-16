9dbd912cbdfb7060c5278765cc922973b13c726d
Alex <weiru@hotmail.ca>
2025-05-15 20:02:41 -0400

    Change page to allow for state reliant content

--- client/app/(withNav)/contact/page.tsx
+++ client/app/(withNav)/contact/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import ContactForm from "./component/contact-form";
 import { Metadata } from "next";
-import ContactInformation from "./component/contact-information";
+import BaseContact from "./component/base";
 
 export const metadata: Metadata = {
   title: "Contact Us",
@@ -9,12 +8,7 @@ export const metadata: Metadata = {
 };
 
 const ContactPage = () => {
-  return (
-    <div className="mt-10 mx-4 flex flex-col md:flex-row items-stretch min-h-screen bg-[#FFF9F5] text-[#6B4C3B] md:space-x-8 space-y-8 md:space-y-0 px-6 md:px-12 py-16 rounded-2xl shadow-xl">
-      <ContactInformation />
-      <ContactForm />
-    </div>
-  );
+  return <BaseContact />;
 };
 
 export default ContactPage;
