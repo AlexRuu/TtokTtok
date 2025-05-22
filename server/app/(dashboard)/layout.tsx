a73a20940b5405acc2f2186cdbca5b5b6bf18339
Alex <weiru@hotmail.ca>
2025-05-22 19:05:52 -0400

    Create layout with navbar

---
+++ server/app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/navbar/navbar";
+import "../globals.css";
+
+export default function WithNavLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+    </div>
+  );
+}
