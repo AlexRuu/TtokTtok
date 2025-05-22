ed6920d8e77ad4477decfbab5773c1863c8eca77
Alex <weiru@hotmail.ca>
2025-05-22 19:11:08 -0400

    Moved protected route check to layout

--- server/app/(dashboard)/layout.tsx
+++ server/app/(dashboard)/layout.tsx
@@ -1,11 +1,18 @@
 import Navbar from "@/components/navbar/navbar";
 import "../globals.css";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function WithNavLayout({
+export default async function WithNavLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+  if (!session || session.user.role !== "ADMIN") {
+    redirect("/signin");
+  }
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
--- server/app/(dashboard)/page.tsx
+++ server/app/(dashboard)/page.tsx
@@ -4,11 +4,6 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const AdminPage = async () => {
-  const session = await getServerSession(authOptions);
-
-  if (!session || session.user.role !== "ADMIN") {
-    redirect("/signin");
-  }
   return <div>AdminPage</div>;
 };
 
