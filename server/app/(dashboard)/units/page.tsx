ef9487424b88a297f28b1f51996529490670b53c
Alex <weiru@hotmail.ca>
2025-05-23 20:09:35 -0400

    Update table structure

--- server/app/(dashboard)/units/page.tsx
+++ server/app/(dashboard)/units/page.tsx
@@ -22,12 +22,19 @@ const UnitsPage = async () => {
 
   return (
     <div className="p-4 sm:p-6 space-y-6">
+      <div className="w-full flex justify-end">
+        <Button className="font-semibold bg-indigo-200 hover:bg-indigo-300 text-indigo-900 flex items-center justify-center gap-2 py-4 sm:py-5 text-base rounded-xl transition-all duration-200 shadow-xs hover:scale-[1.01] hover:shadow-md active:scale-[0.99] focus:outline-hidden focus:ring-2 focus:ring-indigo-300 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-400">
+          <Link href={"/units/create"}>Create</Link>
+        </Button>
+      </div>
       <div className="hidden md:block overflow-auto border rounded-lg">
         <Table className="min-w-full text-sm text-left">
           <TableHeader className="bg-muted text-muted-foreground">
-            <TableHead className="px-4 py-2">Number</TableHead>
-            <TableHead className="px-4 py-2">Name</TableHead>
-            <TableHead className="px-4 py-2">Actions</TableHead>
+            <TableRow>
+              <TableHead className="px-4 py-2">Number</TableHead>
+              <TableHead className="px-4 py-2">Name</TableHead>
+              <TableHead className="px-4 py-2">Actions</TableHead>
+            </TableRow>
           </TableHeader>
           <TableBody className="bg-background divide-y">
             {units.map((unit) => (
@@ -36,7 +43,7 @@ const UnitsPage = async () => {
                 <TableCell className="px-4 py-2">{unit.title}</TableCell>
                 <TableCell className="px-4 py-2 space-x-2">
                   <Button size="sm" variant="outline">
-                    <Link href={`/units/`}>Edit</Link>
+                    <Link href={`/units/${unit.id}`}>Edit</Link>
                   </Button>
                   <Button
                     size="sm"
