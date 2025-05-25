a5b72424959b5ebc5d8a590432d1465a50521d8e
Alex <weiru@hotmail.ca>
2025-05-25 15:58:57 -0400

    Create Units page

--- client/app/(withNav)/lessons/page.tsx
+++ client/app/(withNav)/lessons/page.tsx
@@ -1,32 +1,31 @@
 import getUnits from "@/actions/get-units";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { AccordionContent } from "@radix-ui/react-accordion";
-import React from "react";
+import { Accordion, AccordionItem } from "@/components/ui/accordion";
 
 const LessonsPage = async () => {
   const units = await getUnits();
 
   return (
-    <div>
-      {units.map((unit: { unitNumber: number; title: string }) => (
-        <Accordion type="single" key={unit.unitNumber} collapsible>
-          <AccordionItem value={unit.title}>
-            <AccordionTrigger>
+    <div className="min-h-screen bg-[#FFF9F5] text-[#6B4C3B] mt-10 rounded-xl shadow-md pb-20">
+      <div className="w-11/12 mx-auto pt-5">
+        <h1 className="text-2xl sm:text-3xl font-semibold mb-6 text-center text-[#6B4C3B]">
+          Lessons
+        </h1>
+        <Accordion defaultValue="">
+          {units.map((unit: { unitNumber: number; title: string }) => (
+            <AccordionItem
+              key={unit.unitNumber}
+              value={unit.title}
+              title={
+                <span className="text-lg sm:text-xl font-medium text-[#6B4C3B]">
+                  {unit.unitNumber}. {unit.title}
+                </span>
+              }
+            >
               {unit.unitNumber}. {unit.title}
-            </AccordionTrigger>
-            <AccordionContent>
-              lesson name
-              {/* {unit.lesson.map((lesson) => (
-                <></>
-              ))} */}
-            </AccordionContent>
-          </AccordionItem>
+            </AccordionItem>
+          ))}
         </Accordion>
-      ))}
+      </div>
     </div>
   );
 };
