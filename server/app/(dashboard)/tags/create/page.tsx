26e6fbccffc1ce6729a3567796cc29e4e1bdffb3
Alex <weiru@hotmail.ca>
2025-05-29 16:49:57 -0400

    Create create tag page

---
+++ server/app/(dashboard)/tags/create/page.tsx
@@ -0,0 +1,11 @@
+import CreateTagForm from "./components/create-tag-form";
+
+const CreateUnitPage = () => {
+  return (
+    <div>
+      <CreateTagForm />
+    </div>
+  );
+};
+
+export default CreateUnitPage;
