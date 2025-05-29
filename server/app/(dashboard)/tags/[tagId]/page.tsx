6100fe937f4b39d3dd7ec92fb7df7ecb88bb7d4f
Alex <weiru@hotmail.ca>
2025-05-29 16:49:33 -0400

    Create edit unit page

---
+++ server/app/(dashboard)/tags/[tagId]/page.tsx
@@ -0,0 +1,30 @@
+import prismadb from "@/lib/prismadb";
+import React from "react";
+import EditUnitsForm from "./components/edit-units-form";
+
+type EditUnitsFormProp = {
+  params: {
+    unitId: string;
+  };
+};
+
+const EditUnitsFormPage = async ({ params }: EditUnitsFormProp) => {
+  const { unitId } = await params;
+  const unit = await prismadb.unit.findUnique({
+    where: {
+      id: unitId,
+    },
+  });
+
+  if (!unit) {
+    return <div>Unit not found.</div>;
+  }
+
+  return (
+    <div>
+      <EditUnitsForm initialData={unit} />
+    </div>
+  );
+};
+
+export default EditUnitsFormPage;
