64617faae3aecea406afcc78ee38733d1d0809bf
Alex <weiru@hotmail.ca>
2025-05-31 19:09:19 -0400

    Begin working on dashboard

--- server/app/(dashboard)/page.tsx
+++ server/app/(dashboard)/page.tsx
@@ -1,10 +1,14 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import React from "react";
+import SummaryComponent from "@/components/dashboard/summary";
 
 const AdminPage = async () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="min-h-screen bg-[#FFF9F5] text-[#6B4C3B] rounded-xl shadow-md pb-20">
+      <h1 className="text-2xl font-bold p-10">Overview</h1>
+      <div className="p-10 mx-10 border rounded-2xl shadow-md">
+        <SummaryComponent />
+      </div>
+    </div>
+  );
 };
 
 export default AdminPage;
