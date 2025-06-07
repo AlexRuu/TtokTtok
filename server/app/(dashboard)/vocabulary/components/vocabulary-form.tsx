bcfeb0e0ac21bff7d3bf7ae05538fa4bfed6ff6a
Alex <weiru@hotmail.ca>
2025-06-07 02:34:00 -0400

    Refactor to match new schema

--- server/app/(dashboard)/vocabulary/components/vocabulary-form.tsx
+++ server/app/(dashboard)/vocabulary/components/vocabulary-form.tsx
@@ -20,19 +20,24 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { vocabularyAction } from "@/formActions/form-actions";
 import useLoading from "@/hooks/use-loading";
-import { Lesson, Unit, Vocabulary } from "@/lib/generated/prisma";
-import { cn } from "@/lib/utils";
+import {
+  Lesson,
+  Unit,
+  Vocabulary,
+  VocabularyList,
+} from "@/lib/generated/prisma";
 import {
   vocabularySchema,
   vocabularySchemaValues,
 } from "@/schemas/form-schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
 interface VocabularyFormProps {
-  initialData: Vocabulary | null;
+  initialData: (VocabularyList & { vocabulary: Vocabulary[] }) | null;
   lessons: ({ unit: Unit } & Lesson)[];
 }
 
@@ -42,17 +47,19 @@ const VocabularyForm: React.FC<VocabularyFormProps> = ({
 }) => {
   const { isLoading, startLoading, stopLoading } = useLoading();
   const router = useRouter();
-
   const form = useForm<vocabularySchemaValues>({
     resolver: zodResolver(vocabularySchema),
     defaultValues: initialData
       ? { ...initialData }
       : {
-          english: "",
-          korean: "",
-          definition: "",
+          vocabulary: [{ english: "", korean: "", definition: "" }],
           lessonId: "",
         },
+  });
+
+  const vocabArray = useFieldArray({
+    name: "vocabulary",
+    control: form.control,
   });
 
   const onSubmit: SubmitHandler<vocabularySchemaValues> = async (data) => {
@@ -81,7 +88,7 @@ const VocabularyForm: React.FC<VocabularyFormProps> = ({
   };
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-[#fdfaf6] -mt-10 overflow-auto flex-col">
+    <div className="min-h-screen w-full max-w-screen bg-[#fdfaf6] overflow-x-hidden flex items-center justify-center flex-col py-10 px-4">
       {isLoading && <Loader />}
       <Form {...form}>
         <form
@@ -89,150 +96,135 @@ const VocabularyForm: React.FC<VocabularyFormProps> = ({
           onSubmit={form.handleSubmit(onSubmit)}
           className="w-full max-w-md bg-white border border-pink-100 shadow-xs rounded-2xl p-8 pt-10 space-y-6"
         >
-          <h1 className="text-center text-xl md:text-2xl font-semibold py-2 sm:py-3 md:py-4">
+          <h1 className="text-center text-xl md:text-2xl font-semibold">
             {initialData ? "Edit Vocabulary" : "Create Vocabulary"}
           </h1>
-          <div className="relative">
-            <FormField
-              control={form.control}
-              name="lessonId"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Lesson</FormLabel>
-                  <Select
-                    disabled={isLoading}
-                    onValueChange={field.onChange}
-                    value={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger className="w-full rounded-md border border-gray-300 bg-transparent px-3 py-5 text-base shadow-xs placeholder-transparent focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-indigo-300">
-                        <SelectValue placeholder="Select a Lesson" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {lessons.map((lesson) => (
-                        <SelectItem key={lesson.id} value={lesson.id}>
-                          Unit. {lesson.unit.unitNumber} - {lesson.title}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="flex sm:flex-row sm:space-x-6 space-y-6 sm:space-y-0">
-            <div className="relative w-full">
-              <FormField
-                control={form.control}
-                name="english"
-                disabled={isLoading}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel
-                      htmlFor="english"
-                      className={cn(
-                        form.formState.errors.english &&
-                          form.formState.isSubmitted
-                          ? "text-red-400! before:text-red-400"
-                          : ""
-                      )}
-                    >
-                      English Word
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        id="korean"
-                        {...field}
-                        type="text"
-                        onInvalid={(e) => e.preventDefault()}
-                        className="w-full rounded-md border border-gray-300 bg-transparent px-3 py-5 text-base shadow-xs placeholder-transparent focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-indigo-300"
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="relative w-full">
-              <FormField
-                control={form.control}
-                name="korean"
-                disabled={isLoading}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel
-                      htmlFor="korean"
-                      className={cn(
-                        form.formState.errors.korean &&
-                          form.formState.isSubmitted
-                          ? "text-red-400! before:text-red-400"
-                          : ""
-                      )}
-                    >
-                      Korean Word
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        id="korean"
-                        {...field}
-                        type="text"
-                        onInvalid={(e) => e.preventDefault()}
-                        className="w-full rounded-md border border-gray-300 bg-transparent px-3 py-5 text-base shadow-xs placeholder-transparent focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-indigo-300"
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
-          <div className="relative">
-            <FormField
-              control={form.control}
-              name="definition"
-              disabled={isLoading}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel
-                    htmlFor="definition"
-                    className={cn(
-                      form.formState.errors.definition &&
-                        form.formState.isSubmitted
-                        ? "text-red-400! before:text-red-400"
-                        : ""
-                    )}
-                  >
-                    Definition
-                  </FormLabel>
+
+          <FormField
+            control={form.control}
+            name="lessonId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Lesson</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  value={field.value}
+                  disabled={isLoading}
+                >
                   <FormControl>
-                    <Textarea
-                      id="definition"
-                      {...field}
-                      onInvalid={(e) => e.preventDefault()}
-                      className="w-full rounded-md border border-gray-300 bg-transparent px-3 text-base shadow-xs placeholder-transparent focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-indigo-300"
-                    />
+                    <SelectTrigger className="w-full px-3 py-5 text-base border rounded-md border-gray-300 shadow-xs focus-visible:ring-2 focus-visible:ring-indigo-300">
+                      <SelectValue placeholder="Select a Lesson" />
+                    </SelectTrigger>
                   </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="relative flex w-full space-x-4">
+                  <SelectContent>
+                    {lessons.map((lesson) => (
+                      <SelectItem key={lesson.id} value={lesson.id}>
+                        Unit. {lesson.unit.unitNumber} - {lesson.title}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+
+          <div className="space-y-6">
+            {vocabArray.fields.map((field, index) => (
+              <div key={field.id} className="border-t pt-6 space-y-4">
+                <div className="flex justify-between items-center">
+                  <FormLabel>Vocabulary {index + 1}</FormLabel>
+                  <Button
+                    type="button"
+                    size="icon"
+                    variant="ghost"
+                    className="w-1/8 font-semibold bg-pink-200 hover:bg-pink-300 text-pink-900 flex items-center justify-center gap-2 hover:cursor-pointer text-base sm:text-md rounded-xl transition-colors shadow-xs hover:scale-[1.01] hover:shadow-md duration-200 ease-in-out focus:ring-2 focus:ring-pink-300 active:scale-[0.99] focus:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-pink-400"
+                    onClick={() => vocabArray.remove(index)}
+                  >
+                    <Trash />
+                  </Button>
+                </div>
+
+                <div className="flex space-x-4">
+                  <FormField
+                    control={form.control}
+                    name={`vocabulary.${index}.english`}
+                    disabled={isLoading}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>English Word</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type="text"
+                            className="w-full px-3 py-5 text-base border rounded-md border-gray-300 shadow-xs focus-visible:ring-2 focus-visible:ring-indigo-300"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name={`vocabulary.${index}.korean`}
+                    disabled={isLoading}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Korean Word</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type="text"
+                            className="w-full px-3 py-5 text-base border rounded-md border-gray-300 shadow-xs focus-visible:ring-2 focus-visible:ring-indigo-300"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <FormField
+                  control={form.control}
+                  name={`vocabulary.${index}.definition`}
+                  disabled={isLoading}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Definition</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          {...field}
+                          placeholder="Definition of the word..."
+                          className="w-full px-3 py-4 text-base border rounded-md border-gray-300 shadow-xs focus-visible:ring-2 focus-visible:ring-indigo-300"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            ))}
             <Button
-              className="w-full font-semibold bg-pink-200 hover:bg-pink-300 text-pink-900 flex items-center justify-center gap-2 hover:cursor-pointer py-4 sm:py-5 text-base sm:text-md rounded-xl transition-colors shadow-xs hover:scale-[1.01] hover:shadow-md duration-200 ease-in-out focus:ring-2 focus:ring-pink-300 active:scale-[0.99] focus:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-pink-400"
-              aria-label="Cancel"
+              type="button"
+              onClick={() =>
+                vocabArray.append({ english: "", korean: "", definition: "" })
+              }
+              className="w-full font-semibold bg-indigo-200 hover:bg-indigo-300 text-indigo-900 py-4 rounded-xl shadow-xs hover:scale-[1.01] hover:shadow-md active:scale-[0.99]"
+            >
+              Add Block
+            </Button>
+          </div>
+
+          <div className="flex space-x-4">
+            <Button
               type="button"
               onClick={() => router.push("/vocabulary")}
               disabled={form.formState.isSubmitting}
-              aria-live="assertive"
+              className="w-full font-semibold bg-pink-200 hover:bg-pink-300 text-pink-900 py-4 rounded-xl shadow-xs hover:scale-[1.01] hover:shadow-md active:scale-[0.99] hover:cursor-pointer"
             >
               Cancel
             </Button>
             <Button
               type="submit"
-              className="w-full font-semibold bg-indigo-200 hover:bg-indigo-300 text-indigo-900 flex items-center justify-center gap-2 py-4 sm:py-5 text-base rounded-xl transition-all duration-200 shadow-xs hover:scale-[1.01] hover:shadow-md active:scale-[0.99] focus:outline-hidden focus:ring-2 focus:ring-indigo-300 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-400"
-              aria-label="Submit"
-              aria-busy={form.formState.isSubmitting}
               disabled={form.formState.isSubmitting}
-              aria-live="assertive"
+              className="w-full font-semibold bg-indigo-200 hover:bg-indigo-300 text-indigo-900 py-4 rounded-xl shadow-xs hover:scale-[1.01] hover:shadow-md active:scale-[0.99] hover:cursor-pointer"
             >
               {form.formState.isSubmitting ? "Submitting..." : "Submit"}
             </Button>
