550ecaec546ba837aa8fa30007edac636d035e38
Alex <weiru@hotmail.ca>
2025-06-10 03:36:50 -0400

    Create create quiz page

---
+++ server/app/(dashboard)/quizzes/create/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import QuizForm from "../components/quiz-form";
+import { findLessons } from "@/prisma/prismaFetches";
+
+const CreateQuiz = async () => {
+  const lessons = await findLessons();
+
+  return (
+    <div>
+      <QuizForm initialData={null} lessons={lessons} />
+    </div>
+  );
+};
+
+export default CreateQuiz;
