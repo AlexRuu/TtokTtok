b059a4a9ba9d0cdd8414276706619bac5c04cae8
Alex <weiru@hotmail.ca>
2025-06-21 21:33:26 -0400

    Update action buttons

--- server/app/(dashboard)/units/page.tsx
+++ server/app/(dashboard)/units/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
 import DeleteButton from "@/components/ui/delete-button";
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
   Table,
   TableBody,
   TableCell,
@@ -10,7 +15,7 @@ import {
 } from "@/components/ui/table";
 
 import { findAscUnits } from "@/prisma/prismaFetches";
-import { Edit } from "lucide-react";
+import { Edit, Ellipsis } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -23,17 +28,30 @@ const UnitsPage = async () => {
 
   return (
     <div className="p-4 sm:p-6 space-y-6">
-      <div className="w-full flex justify-end">
-        <Button className="font-semibold bg-indigo-200 hover:bg-indigo-300 text-indigo-900 flex items-center justify-center gap-2 py-4 sm:py-5 text-base rounded-xl transition-all duration-200 shadow-xs hover:scale-[1.01] hover:shadow-md active:scale-[0.99] focus:outline-hidden focus:ring-2 focus:ring-indigo-300 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-400">
-          <Link href={"/units/create"}>+ Create</Link>
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
+        <div>
+          <h1 className="text-3xl font-bold tracking-tight">Units</h1>
+          <p className="text-muted-foreground text-sm sm:text-base">
+            Overview of all units
+          </p>
+        </div>
+        <Button
+          asChild
+          className="bg-indigo-100 hover:bg-indigo-200 text-indigo-900 font-medium px-5 py-3 text-base rounded-xl shadow-sm transition-all hover:scale-[1.01] focus:ring-2 focus:ring-indigo-300"
+        >
+          <Link href="/units/create">+ Create Unit</Link>
         </Button>
       </div>
+
       <div className="hidden md:block overflow-auto border rounded-lg">
         <Table className="min-w-full text-sm text-left">
           <TableHeader className="bg-muted text-muted-foreground">
             <TableRow>
               <TableHead className="px-4 py-2 text-center">Number</TableHead>
               <TableHead className="px-4 py-2 text-center">Name</TableHead>
+              <TableHead className="px-4 py-2 text-center">
+                Number of Lessons
+              </TableHead>
               <TableHead className="px-4 py-2 text-center">Actions</TableHead>
             </TableRow>
           </TableHeader>
@@ -46,31 +64,35 @@ const UnitsPage = async () => {
                 <TableCell className="px-4 py-2 text-center">
                   {unit.title}
                 </TableCell>
-                <TableCell className="px-4 py-2 space-x-2">
-                  <div className="w-1/4 gap-5 flex mx-auto">
-                    <Button
-                      size="sm"
-                      variant="outline"
-                      className="bg-indigo-100 hover:bg-indigo-200 text-indigo-900 font-medium flex items-center justify-center gap-2 py-4 text-base sm:text-md rounded-xl shadow-sm transition-all hover:scale-[1.01] focus:ring-2 focus:ring-indigo-300 hover:cursor-pointer sm:py-5 sm:text-md hover:shadow-md duration-200 ease-in-out"
-                      asChild
+                <TableCell className="px-4 py-2 text-center">
+                  {unit.lesson.length}
+                </TableCell>
+                <TableCell className="px-4 py-2 text-center">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className="hover:cursor-pointer">
+                      <Ellipsis />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                      className="w-full py-3 px-4 flex-col flex gap-2"
+                      align="end"
                     >
-                      <Link
-                        href={`/units/${unit.id}`}
-                        className="flex items-center"
+                      <Button
+                        asChild
+                        className="bg-indigo-100 hover:bg-indigo-200 text-indigo-900 font-medium px-5 py-3 text-base rounded-xl shadow-sm hover:shadow-md transition-all hover:scale-[1.01] focus:ring-2 focus:ring-indigo-300 hover:cursor-pointer"
                       >
-                        <Edit />
-                        Edit
-                      </Link>
-                    </Button>
-                    <DeleteButton path="units" id={unit.id} />
-                  </div>
+                        <Link href={`/units/${unit.id}`}>
+                          <Edit /> Edit
+                        </Link>
+                      </Button>
+                      <DeleteButton path="units" id={unit.id} />
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </div>
-
       {/* Mobile View */}
       <div className="md:hidden space-y-4">
         {units.map((unit) => (
