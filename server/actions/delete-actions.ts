3149d1ab194f50f4bd50845ac42e30fbf20fc21e
Alex <weiru@hotmail.ca>
2025-07-04 01:03:40 -0400

    Add success toast and deletedRequestedUsers function

--- server/actions/delete-actions.ts
+++ server/actions/delete-actions.ts
@@ -21,6 +21,37 @@ const deleteItem = async (path: string, id: string) => {
     toastError(path);
     return;
   }
+  toast.success(`Deleted successfully.`, {
+    style: {
+      background: "#e6f9f0",
+      color: "#1a7f5a",
+      borderRadius: "10px",
+      padding: "12px 18px",
+      boxShadow: "0 4px 6px rgba(0, 0, 0, 0.08)",
+      fontSize: "16px",
+    },
+    className: "transition-all transform duration-300 ease-in-out font-medium",
+  });
 };
 
-export { deleteItem };
+const deleteRequestedUsers = async () => {
+  const URL = "/api/user/deleteInactive";
+  const res = await fetch(URL, { method: "DELETE" });
+  if (!res.ok) {
+    toastError("users");
+    return;
+  }
+  toast.success(`Deleted successfully.`, {
+    style: {
+      background: "#e6f9f0",
+      color: "#1a7f5a",
+      borderRadius: "10px",
+      padding: "12px 18px",
+      boxShadow: "0 4px 6px rgba(0, 0, 0, 0.08)",
+      fontSize: "16px",
+    },
+    className: "transition-all transform duration-300 ease-in-out font-medium",
+  });
+};
+
+export { deleteItem, deleteRequestedUsers };
