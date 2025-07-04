8059f9876d6ad32d2c5c8add6d33ecc12cfbf558
Alex <weiru@hotmail.ca>
2025-07-04 01:06:09 -0400

    Component refactor

--- server/app/(dashboard)/users/page.tsx
+++ server/app/(dashboard)/users/page.tsx
@@ -1,25 +1,11 @@
 import { findUsers } from "@/prisma/prismaFetches";
-import { UserTable } from "./components/user-table";
 import { columns } from "./components/columns";
+import UserManagementDisplay from "./components/user-management-display";
 
 const AdminUsersPage = async () => {
   const users = await findUsers();
-  const deleteRequested = users.filter((user) => user.status === "INACTIVE");
-  const activeUsers = users.filter((user) => user.status === "ACTIVE");
 
-  return (
-    <div className="p-4 sm:p-6 space-y-6">
-      <h1 className="text-2xl font-semibold">User Management</h1>
-      {deleteRequested.length > 0 && (
-        <UserTable
-          data={deleteRequested}
-          columns={columns}
-          title="Deletion Requested"
-        />
-      )}
-      <UserTable data={activeUsers} columns={columns} title="Active Users" />
-    </div>
-  );
+  return <UserManagementDisplay users={users} columns={columns} />;
 };
 
 export default AdminUsersPage;
