fb8ec4a58eebb4194486ac8a490d2372090a1016
Alex Ru <weiru@hotmail.ca>
2025-07-07 23:30:36 -0400

    Begin altering to use slug for lesson

--- client/app/(withNav)/lessons/page.tsx
+++ client/app/(withNav)/lessons/page.tsx
@@ -1,9 +1,11 @@
 import getUnits from "@/actions/get-units";
 import { Accordion, AccordionItem } from "@/components/ui/accordion";
+import { Unit } from "@/types";
+import Link from "next/link";
 
 const LessonsPage = async () => {
-  const units = await getUnits();
-
+  const units = (await getUnits()) as Unit[];
+  console.log(units);
   return (
     <div className="min-h-screen bg-[#FFF9F5] text-[#6B4C3B] mt-10 rounded-xl shadow-md pb-20">
       <div className="w-11/12 mx-auto pt-5">
@@ -11,7 +13,7 @@ const LessonsPage = async () => {
           Lessons
         </h1>
         <Accordion defaultValue="">
-          {units.map((unit: { unitNumber: number; title: string }) => (
+          {units.map((unit) => (
             <AccordionItem
               key={unit.unitNumber}
               value={unit.title}
@@ -21,7 +23,11 @@ const LessonsPage = async () => {
                 </span>
               }
             >
-              {unit.unitNumber}. {unit.title}
+              {unit.lesson.map((lesson) => (
+                <Link href={`/lessons/${lesson.slug}`} key={lesson.id}>
+                  {lesson.lessonNumber}. {lesson.title}
+                </Link>
+              ))}
             </AccordionItem>
           ))}
         </Accordion>
