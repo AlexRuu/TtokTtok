0c584c53feaf555499d719c616f56368a74f341b
Alex Ru <weiru@hotmail.ca>
2025-07-09 02:44:52 -0400

    Separate the units/lessons ui into different component

--- client/app/(withNav)/lessons/page.tsx
+++ client/app/(withNav)/lessons/page.tsx
@@ -1,36 +1,18 @@
 import getUnits from "@/actions/get-units";
-import { Accordion, AccordionItem } from "@/components/ui/accordion";
 import { Unit } from "@/types";
-import Link from "next/link";
+import GroupedLessons from "./components/grouped-lessons";
 
 const LessonsPage = async () => {
   const units = (await getUnits()) as Unit[];
-  console.log(units);
+
   return (
     <div className="min-h-screen bg-[#FFF9F5] text-[#6B4C3B] mt-10 rounded-xl shadow-md pb-20">
-      <div className="w-11/12 mx-auto pt-5">
-        <h1 className="text-2xl sm:text-3xl font-semibold mb-6 text-center text-[#6B4C3B]">
+      <div className="w-11/12 max-w-4xl mx-auto pt-5">
+        <h1 className="text-3xl font-semibold mb-8 text-center tracking-wide">
           Lessons
         </h1>
-        <Accordion defaultValue="">
-          {units.map((unit) => (
-            <AccordionItem
-              key={unit.unitNumber}
-              value={unit.title}
-              title={
-                <span className="text-lg sm:text-xl font-medium text-[#6B4C3B]">
-                  {unit.unitNumber}. {unit.title}
-                </span>
-              }
-            >
-              {unit.lesson.map((lesson) => (
-                <Link href={`/lessons/${lesson.slug}`} key={lesson.id}>
-                  {lesson.lessonNumber}. {lesson.title}
-                </Link>
-              ))}
-            </AccordionItem>
-          ))}
-        </Accordion>
+
+        <GroupedLessons units={units} />
       </div>
     </div>
   );
