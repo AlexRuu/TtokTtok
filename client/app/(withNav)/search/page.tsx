97a248b02a7c4da89bcea2e30b25e937cab6b881
Alex <weiru@hotmail.ca>
2025-07-19 19:58:50 -0400

    Update metadata

--- client/app/(withNav)/search/page.tsx
+++ client/app/(withNav)/search/page.tsx
@@ -9,7 +9,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Search",
-  description: "Search Ttok Ttok",
+  description: "Search TtokTtok",
 };
 
 interface SearchPageProps {
