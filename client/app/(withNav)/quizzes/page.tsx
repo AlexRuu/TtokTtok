1c47e0eb1dfebeaeb02558a3f743dd72aa28dbe8
Alex <weiru@hotmail.ca>
2025-08-10 19:09:40 -0400

    Create Quiz page

---
+++ client/app/(withNav)/quizzes/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Quizzes",
+  description: "TtokTtok Quizzes",
+};
+
+const QuizPage = () => {
+  return (
+    <div className="mx-auto mt-10 max-w-4xl w-full px-4 sm:px-6 py-14 bg-[#FFF9F5] text-[#6B4C3B] rounded-2xl shadow-md space-y-12 mb-10">
+      QuizPage
+    </div>
+  );
+};
+
+export default QuizPage;
