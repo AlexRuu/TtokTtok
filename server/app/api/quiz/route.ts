e97b3af1b2c537414539594378fda7aca4a0ea2c
Alex <weiru@hotmail.ca>
2025-08-21 19:06:06 -0400

    Update POST route and add GET route

--- server/app/api/quiz/route.ts
+++ server/app/api/quiz/route.ts
@@ -1,8 +1,6 @@
 import { authOptions } from "@/lib/auth";
-import { Tag } from "@/lib/generated/prisma";
 import { withRls } from "@/lib/withRLS";
 import { quizSchema } from "@/schemas/form-schemas";
-import { Prisma } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
@@ -47,55 +45,74 @@ export async function POST(req: Request) {
         return new NextResponse("Lesson does not exist", { status: 409 });
       }
 
-      await tx.$transaction(async (trx: Prisma.TransactionClient) => {
-        const createdQuiz = await trx.quiz.create({
+      const createdQuiz = await tx.quiz.create({
+        data: {
+          title,
+          lesson: { connect: { id: lessonId } },
+          quizQuestion: {
+            create: quizQuestion.map((q) => ({
+              question: q.question,
+              quizType: q.quizType,
+              options:
+                q.quizType === "MULTIPLE_CHOICE" || q.quizType === "MATCHING"
+                  ? q.options
+                  : undefined,
+              answer:
+                typeof q.answer === "string"
+                  ? q.answer
+                  : JSON.stringify(q.answer),
+            })),
+          },
+        },
+      });
+
+      const uniqueTagNames = Array.from(
+        new Set(
+          quizQuestion.map((q) => quizTypeToTagMap[q.quizType]).filter(Boolean)
+        )
+      );
+
+      const tags = await tx.tag.findMany({
+        where: { name: { in: uniqueTagNames } },
+      });
+
+      for (const tag of tags) {
+        await tx.tagging.create({
           data: {
-            title,
-            lesson: { connect: { id: lessonId } },
-            quizQuestion: {
-              create: quizQuestion.map((q) => ({
-                question: q.question,
-                quizType: q.quizType,
-                options:
-                  q.quizType === "MULTIPLE_CHOICE" || q.quizType === "MATCHING"
-                    ? q.options
-                    : undefined,
-                answer:
-                  typeof q.answer === "string"
-                    ? q.answer
-                    : JSON.stringify(q.answer),
-              })),
-            },
+            tagId: tag.id,
+            quizId: createdQuiz.id,
           },
         });
-
-        const uniqueTagNames = Array.from(
-          new Set(
-            quizQuestion
-              .map((q) => quizTypeToTagMap[q.quizType])
-              .filter(Boolean)
-          )
-        );
-
-        const tags = await trx.tag.findMany({
-          where: { name: { in: uniqueTagNames } },
-        });
-
-        for (const tag of tags) {
-          await trx.tagging.create({
-            data: {
-              tagId: tag.id,
-              quizId: createdQuiz.id,
-            },
-          });
-        }
-      });
+      }
 
       return new NextResponse("Quiz created successfully", { status: 200 });
     });
   } catch (error) {
     console.error("Error creating quiz:", error);
     return new NextResponse("There was an error creating the quiz", {
+      status: 500,
+    });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+    return await withRls(session, async (tx) => {
+      const quizzes = await tx.quiz.findMany({
+        include: {
+          quizQuestion: true,
+          lesson: { include: { unit: true } },
+          tagging: { include: { tag: true } },
+        },
+        orderBy: { lesson: { unit: { unitNumber: "asc" } } },
+      });
+
+      return NextResponse.json(quizzes);
+    });
+  } catch (error) {
+    console.error("Error fetching quizzes:", error);
+    return new NextResponse("There was an error fetching the quizzes", {
       status: 500,
     });
   }
