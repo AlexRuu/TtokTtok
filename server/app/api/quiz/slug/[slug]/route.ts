606c3fb585e4839a32113f78d6e6627344e1b950
Alex <weiru@hotmail.ca>
2025-08-24 20:30:32 -0400

    Create fetch by slug

---
+++ server/app/api/quiz/slug/[slug]/route.ts
@@ -0,0 +1,37 @@
+import { authOptions } from "@/lib/auth";
+import { withRls } from "@/lib/withRLS";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _req: Request,
+  props: { params: Promise<{ slug: string }> }
+) {
+  const params = await props.params;
+  try {
+    const session = await getServerSession(authOptions);
+    return await withRls(session, async (tx) => {
+      const quiz = await tx.quiz.findUnique({
+        where: {
+          slug: params.slug,
+        },
+        include: {
+          quizQuestion: true,
+          lesson: { include: { unit: true } },
+          tagging: { include: { tag: true } },
+        },
+      });
+
+      if (!quiz) {
+        return new NextResponse("Quiz not found", { status: 404 });
+      }
+
+      return NextResponse.json(quiz);
+    });
+  } catch (error) {
+    console.error("Error finding specific quiz by slug", error);
+    return new NextResponse("Error finding specific quiz by slug", {
+      status: 500,
+    });
+  }
+}
