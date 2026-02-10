751910c86a6bf10725c84aebe1b8c842b1e9b391
Alex <weiru@hotmail.ca>
2026-02-10 16:50:37 -0500

    Add metadata

--- client/app/(withNav)/units/[unitsSlug]/lessons/[lessonSlug]/vocabulary/page.tsx
+++ client/app/(withNav)/units/[unitsSlug]/lessons/[lessonSlug]/vocabulary/page.tsx
@@ -1,6 +1,21 @@
 import { notFound } from "next/navigation";
 import VocabularyClient from "./components/vocabulary-client";
 import getVocabulary from "@/actions/get-vocabulary";
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ lessonSlug: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { lessonSlug } = await params;
+  const fetchedVocabulary = await getVocabulary(lessonSlug);
+
+  return {
+    title: fetchedVocabulary.title,
+    description: `${fetchedVocabulary.title} vocabulary page`,
+  };
+}
 
 const LessonVocabularyPage = async ({
   params,
