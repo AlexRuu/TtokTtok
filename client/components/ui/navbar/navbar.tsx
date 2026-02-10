a43fd205e1ac323aa12ebffb3e70cf9791e4488b
Alex <weiru@hotmail.ca>
2026-02-10 16:51:09 -0500

    Update to add link pathway to vocabulary list page

--- client/components/ui/navbar/navbar.tsx
+++ client/components/ui/navbar/navbar.tsx
@@ -69,6 +69,7 @@ export default function Navbar() {
       { name: "About", path: "/about" },
       { name: "Units", path: "/units" },
       { name: "Quizzes", path: "/quizzes" },
+      { name: "Vocabulary", path: "/vocabulary" },
       { name: "Tags", path: "/tags" },
     ],
     [],
