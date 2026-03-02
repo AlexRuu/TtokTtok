8257ffa03ec1c2a8d2b686da587a4c97a6bda5f7
Alex <weiru@hotmail.ca>
2026-03-01 19:11:35 -0500

    Add error handling

--- client/actions/get-tags.tsx
+++ client/actions/get-tags.tsx
@@ -3,6 +3,10 @@ const URL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/tags`;
 const getTags = async () => {
   try {
     const res = await fetch(`${URL}`);
+    if (!res.ok) {
+      console.log("Error fetching tags.");
+      return null;
+    }
     const tags = res.json();
     return tags;
   } catch (error) {
