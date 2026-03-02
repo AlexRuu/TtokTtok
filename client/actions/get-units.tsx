98d06ed05c5365cc7386e0cd22a0a9dfbd7da7dc
Alex <weiru@hotmail.ca>
2026-03-01 19:11:42 -0500

    Add error handling

--- client/actions/get-units.tsx
+++ client/actions/get-units.tsx
@@ -3,6 +3,11 @@ const URL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/units`;
 const getUnits = async () => {
   try {
     const res = await fetch(`${URL}`);
+    if (!res.ok) {
+      console.log("Error fetching units");
+      return null;
+    }
+
     const units = res.json();
     return units;
   } catch (error) {
